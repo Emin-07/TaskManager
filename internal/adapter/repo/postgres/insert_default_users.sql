6811fdf4be2d7a83a419857f49bd1faa403277ea
Mohmad Saidov <moxmad_saidov@mail.ru>
2026-07-12 20:12:32 +0300

    removed trash

--- internal/adapter/repo/postgres/insert_default_users.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (username, role, email, password_hash) VALUES
-                                                             ('admin', 'admin', 'admin@email.com', 'admin123'),
-                                                             ('test', 'user', 'test@email.com', '12345678'),
-                                                             ('john_doe_2007', 'user', 'johndoe2007@email.com', 'megaSecret');
